0761fbb4358ae757275230a636591875b4cadd7b
Devagya Tak <thegreattak94@gmail.com>
2024-05-16 16:36:54 +0530

    do it yourself 3

--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -12,7 +12,7 @@ const QuestionPage = () => {
     console.log(my_question)
     return (
         <div className='bg-zinc-950 w-screen h-screen flex items-center justify-center'>
-            <div className="question-dabba bg-white min-w-[390px] max-w-80 min-h-[190px] rounded-3xl flex flex-col gap-2 ">
+            <div className="question-dabba bg-white min-w-[390px] max-w-70 min-h-[190px] rounded-3xl flex flex-col gap-2 ">
                 <div className='bg-gradient-to-r from-pink-600 to-purple-600 w-auto rounded-3xl min-h-[90px] flex justify-center items-center text-xl text-white font-semibold gap-1'>Ask me anything anonymously</div>
                 <div className="textbox flex justify-center items-center">
 
